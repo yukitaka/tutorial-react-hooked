51b267ba071503bccebb2b4dceabc60b02d74010
Takayuki Sato <s.yukkiy@gmail.com>
2020-07-11 17:19:57 +0900

    feat: Add search

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import '../App.css';
 import Header from "./Header";
 import Movie from "./Movie";
+import Search from "./Search"
 
 const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  const [errorMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     fetch(MOVIE_API_URL)
@@ -19,9 +20,27 @@ const App = () => {
       });
   }, []);
 
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response == "True") {
+          setMovies(jsonResponse.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
+        }
+      });
+  };
+
   return (
     <div className="App">
       <Header text="HOOKED" />
+      <Search search={search} />
       <p className="App-intro">Sharing a few of our favorite movies</p>
       <div className="movies">
         {loading && !errorMessage ? (
